bf7aaab3d2c27eb7fef2db2c8e4f51f6016f7788
leirdan <uouleirdan@gmail.com>
2022-10-23 15:47:02 -0300

    Initial commit

---
+++ notes.md
@@ -0,0 +1,9 @@
+## Verbos HTTP:
+
+- get, recebe dados de um Resource
+- post, envia dados para serem processados por um Resource
+- put, atualiza dados de um Resource
+- delete, deleta um Resource
+- ...
+  o Resource é o endpoint de uma URI: ex. https://localhost:4040/clients
+  clients é o Resource.
---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const app = express();
+const data = require("./data.json");
+
+app.use(express.json());
+
+app.get("/songs", (req, res) => {
+  res.status(200).json(data);
+});
+app.get("/songs/:id", (req, res) => {
+  const { id } = req.params;
+  const song = data.find((song) => song.id == id);
+
+  if (!song) {
+    return res.status(204).json();
+  }
+
+  res.status(200).json(song);
+});
+
+app.post("songs", (req, res) => {});
+app.put("songs/:id", (req, res) => {
+  const { id } = req.params;
+  const song = data.find((song) => song.id == id);
+
+  if (!song) {
+    return res.status(204).json();
+  }
+  const { title } = req.body;
+  const { composer } = req.body;
+  song.title = title;
+  song.composer = composer;
+
+  res.json(song);
+});
+app.delete("/songs/:id", (req, res) => {
+  const { id } = req.params;
+  const songsFiltered = data.filter((songs) => songs.id != id);
+
+  res.json(songsFiltered);
+});
+
+app.listen(8002, () => {
+  console.log("Servidor aberto.");
+});
